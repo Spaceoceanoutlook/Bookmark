1ab0638f4008b498afb4d140fb251a02db590edb
Valerii <spaceocean@outlook.com>
2025-01-28 19:54:43 +0500

    added flask login

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask_login import LoginManager
+
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
 
+    login_manager.init_app(app)
+
     with app.app_context():
         from . import routes
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # return User.query.get(int(user_id)) !!!!!!!!!!!!!!!!!!!!!!!!!!!
+        return 'OK'
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, current_app as app
+from flask import render_template, request, current_app as app
+from werkzeug.security import generate_password_hash
 
 @app.route('/')
 def index():
@@ -12,8 +13,19 @@ def home():
 def auth():
     return render_template('auth.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        # hashed_password = generate_password_hash(password, method='sha256')
+        # new_user = User(email=email, password=hashed_password)
+        # db.session.add(new_user)
+        # db.session.commit()
+        # flash('Registration successful! You can now log in.', 'success')
+        print(email)
+        print(password)
+        return render_template('home.html')
     return render_template('register.html')
 
 @app.route('/theme')
