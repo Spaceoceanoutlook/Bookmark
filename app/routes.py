fed68aee29131564ec41ff77944efb557c10c4d6
Valerii <spaceocean@outlook.com>
2025-01-29 15:24:07 +0500

    added creating new theme

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from flask import redirect, url_for, render_template, request, current_app as app
-from flask_login import login_required, login_user, logout_user
+from flask import redirect, url_for, render_template, request, current_app as app, jsonify
+from flask_login import login_required, login_user, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import Session
-from .models import User
+from .models import User, Topic, Post
 
 
 @app.route('/')
@@ -13,8 +13,38 @@ def index():
 @app.route('/home')
 @login_required
 def home():
-    return render_template('home.html')
+    session = Session()
+    topics = session.query(Topic).filter_by(user_id=current_user.id).all()
+    session.close()
+    return render_template('home.html', topics=topics)
 
+@app.route('/save_topic', methods=['POST'])
+@login_required
+def save_topic():
+    data = request.json
+    topic_name = data.get('topicName')
+
+    if topic_name:
+        session = Session()
+        new_topic = Topic(name=topic_name, user_id=current_user.id)
+        session.add(new_topic)
+        session.commit()
+        session.refresh(new_topic)
+        session.close()
+
+        return jsonify({"success": True, "topicName": topic_name, "topicId": new_topic.id})
+    else:
+        return jsonify({"success": False, "message": "Topic name is required"})
+
+
+@app.route('/theme/<int:topic_id>')
+@login_required
+def theme(topic_id):
+    session = Session()
+    topic = session.query(Topic).filter_by(id=topic_id, user_id=current_user.id).first()
+    posts = session.query(Post).filter_by(topic_id=topic_id).all()
+    session.close()
+    return render_template('theme.html', topic=topic, posts=posts)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -57,8 +87,3 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
-
-
-@app.route('/theme')
-def theme():
-    return render_template('theme.html')
